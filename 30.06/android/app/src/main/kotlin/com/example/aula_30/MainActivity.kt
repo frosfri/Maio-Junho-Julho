5ecb3574e8f27d0f304d07660e9b26103e60e5fa
LUIZ <luizhenriquedossantos1505@gmail.com>
2026-08-07 10:51:13 -0300

    Add files via upload

---
+++ 30.06/android/app/src/main/kotlin/com/example/aula_30/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula_30
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Atv 31 07/android/app/src/main/kotlin/com/example/atv_31_07/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atv_31_07
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
